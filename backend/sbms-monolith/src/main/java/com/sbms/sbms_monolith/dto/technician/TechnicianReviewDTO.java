672ed4c73825685c20a9af45cf57cf3c6b10469c
Dinuka Vishal <dinukavishal797@gmail.com>
2026-03-13 17:27:25 +0530

    update

--- backend/sbms-monolith/src/main/java/com/sbms/sbms_monolith/dto/technician/TechnicianReviewDTO.java
+++ backend/sbms-monolith/src/main/java/com/sbms/sbms_monolith/dto/technician/TechnicianReviewDTO.java
@@ -8,7 +8,6 @@ import java.time.LocalDate;
 public class TechnicianReviewDTO {
     private Long id;
     private String ownerName;
-    private String ownerProfileImageUrl;
     private int rating;
     private String comment;
     private LocalDate date;
